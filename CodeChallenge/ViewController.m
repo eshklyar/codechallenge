be3d11c1f75533c73414eb895d8419ed75efc59b
eshklyar <eshklyar@gmail.com>
2014-10-02 15:55:17 -0500

    first try

--- CodeChallenge/ViewController.m
+++ CodeChallenge/ViewController.m
@@ -6,9 +6,14 @@
 //  Copyright (c) 2014 Edik Shklyar. All rights reserved.
 //
 
-#import "ViewController.h"
+#import "ViewController.h" 
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UITextField *valueOne;
+@property (weak, nonatomic) IBOutlet UITextField *valueTwo;
+@property (weak, nonatomic) IBOutlet UINavigationItem *navBar;
+@property (weak, nonatomic) IBOutlet UIBarButtonItem *topBar;
+@property (weak, nonatomic) IBOutlet UIWebView *webDisplay;
 
 @end
 
@@ -16,12 +21,46 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+/*  NSURL *url =[NSURL URLWithString:@"http://mobilemakers.co"];
+    NSURLRequest *urlRequest = [NSURLRequest requestWithURL:url];
+    [self.webDisplay loadRequest:urlRequest];
+*/
+    self.topBar.title = @"http://mobilemakers.co";
+}
+
+
+- (IBAction)calValue:(id)sender {
+
+    NSInteger firstNumber = self.valueOne.text.integerValue;
+
+    NSInteger secondNumber = self.valueTwo.text.integerValue;
+
+    NSInteger result = (firstNumber * secondNumber);
+
+    if (result % 5 ==0) {
+        self.topBar.enabled = false;
+    }
+
+    NSString* calResult = [NSString stringWithFormat:@"%ld", (long)result];
+
+    self.navBar.title = calResult;
+    [self.valueOne resignFirstResponder];
+    [self.valueTwo resignFirstResponder];
+    }
+
+- (BOOL) textFieldShouldReturn:(UITextField *)valueOne
+{
+    NSLog(@"value 1");
+    [self.valueOne resignFirstResponder];
+    [self.valueTwo resignFirstResponder];
+    return YES;
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+
+
 }
 
 @end
